b5a11ace41441577eaf07b7e21f0be5c9028a794
nathenchan <63383087@qq.com>
2019-07-09 17:25:24 +0800

    stroke animation

--- examples/components/Index.vue
+++ examples/components/Index.vue
@@ -3,8 +3,7 @@
 		<!-- <p>xinui是一套面向前台页面的常用组件库，适配桌面及移动端</p> -->
 		<!-- 按钮 Loading 弹窗 拼图验证 上传 圆环 -->
 		<!-- 分页、下拉select、无限滑动加载、上拉刷新 -->
-		<svg width="400" height="400">
-			<rect x="0" y="0" width="100" height="100" stroke="#000" stroke-width="2" fill="none"></rect>
-		</svg>
+		
 	</div>
 </template>
+
